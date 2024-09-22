e9042a46e879b437f173f6eb84425ea24f667904
Adarsh Mishra <iamadarsh1994@gmail.com>
2024-09-22 20:43:31 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,47 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Homepage from './components/Homepage'
-import SimpleRegistrationForm from './components/SimpleRegistrationForm'
-import Signin from './components/Signin'
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Homepage from './components/Homepage';
+import SimpleRegistrationForm from './components/SimpleRegistrationForm';
+import Signin from './components/Signin';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Cart from "./pages/Cart";
 import Wishlist from "./pages/Wishlist";
 import Account from "./components/Account";
-import CheckoutForm from "./pages/CheckoutForm"
+import CheckoutForm from "./pages/CheckoutForm";
+
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
       <div className="flex flex-col min-h-screen">
-      <div className="container mx-auto p-4">
-            <Navbar />
-      </div>
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+          <Navbar />
+        </div>
   
-  <div className="flex-1"> 
-    <Routes>
-      <Route path="/signup" element={<SimpleRegistrationForm />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/homepage" element={<Homepage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/account" element={<Account />} />
-      <Route path="/CheckoutForm" element={<CheckoutForm />} />
-    </Routes>
-  </div>
+        <div className="flex-1">
+          <Routes>
+            <Route path="/signup" element={<SimpleRegistrationForm />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/homepage" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/checkout" element={<CheckoutForm />} />
+          </Routes>
+        </div>
 
-  <Footer /> 
-  </div>
-
-  </ BrowserRouter>
-  </>
-  )
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+          <Footer />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,84 +1,72 @@
-// import SimpleRegistrationForm from "./SimpleRegistrationForm";
 import { Link } from "react-router-dom";
-import carticon from '../assets/cart.png'
-import wishicon from '../assets/heart2.png'
-import Accicon from '../assets/user.png'
+import carticon from '../assets/cart.png';
+import wishicon from '../assets/heart2.png';
+import Accicon from '../assets/user.png';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="bg-white shadow-md flex justify-between items-center py-4">
-      
       <a href="#" className="text-lg font-bold">
         Exclusive
       </a>
 
-      
-      <ul className="flex justify-end">
+      <div className="block md:hidden">
+        <button
+          className="focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            className="h-6 w-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+
+      <ul className={`flex-col md:flex md:flex-row md:justify-end ${isOpen ? 'flex' : 'hidden'} absolute md:relative bg-white w-full md:w-auto z-10`}>
         <li className="mr-6">
-          <Link to="/homepage" className="text-gray-600 hover:text-gray-900">
-            Home
+          <Link to="/homepage" className="text-gray-600 hover:text-gray-900">Home</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/about" className="text-gray-600 hover:text-gray-900">About</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/signup" className="text-gray-600 hover:text-gray-900">Sign Up</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/search" className="text-gray-600 hover:text-gray-900">Search</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/wishlist" className="text-gray-600 hover:text-gray-900">
+            <img src={wishicon} alt="Wishlist" className="w-6 h-6" />
           </Link>
         </li>
         <li className="mr-6">
-          <Link to="/about" className="text-gray-600 hover:text-gray-900">
-            About
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link to="/contact"  className="text-gray-600 hover:text-gray-900">
-            Contact
-          </Link>
-        </li>
-      
-        <li className="mr-6">
-          <Link to="/signup"  className="text-gray-600 hover:text-gray-900">
-            Sign Up
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link to="/search"  className="text-gray-600 hover:text-gray-900">
-            Search
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link to="/wishlist"  className="text-gray-600 hover:text-gray-900">
-            <img src={wishicon} alt="Wishlist" className="w-6 h-6"/>
-          </Link>
-        </li>
-        <li className="mr-6">
-          <Link to="/cart"  className="text-gray-600 hover:text-gray-900">
+          <Link to="/cart" className="text-gray-600 hover:text-gray-900">
             <img src={carticon} alt="Cart" className="w-6 h-6" />
           </Link>
         </li>
         <li className="mr-6">
-          <Link to="/account"  className="text-gray-600 hover:text-gray-900">
-            <img src={Accicon} alt="Cart" className="w-6 h-6" />
+          <Link to="/account" className="text-gray-600 hover:text-gray-900">
+            <img src={Accicon} alt="Account" className="w-6 h-6" />
           </Link>
         </li>
         <li className="mr-6">
-          <Link to="/CheckoutForm"  className="text-gray-600 hover:text-gray-900">
-            Checkout
-          </Link>
+          <Link to="/checkout" className="text-gray-600 hover:text-gray-900">Checkout</Link>
         </li>
       </ul>
-
-      
-      <button
-        className="block md:hidden bg-gray-200 hover:bg-gray-300 py-2 px-4"
-        onClick={() => {}}
-      >
-        <svg
-          className="h-6 w-6"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </button>
     </nav>
   );
 };
